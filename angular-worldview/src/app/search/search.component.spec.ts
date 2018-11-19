f9ad215a7d1e8bcafce73e35420d44431bcf0392
trt26@case.edu <trt26@case.edu>
2018-11-18 23:08:11 -0500

    Testing changes

--- angular-worldview/src/app/search/search.component.spec.ts
+++ angular-worldview/src/app/search/search.component.spec.ts
@@ -1,13 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule }   from '@angular/forms';
 import { SearchComponent } from './search.component';
-
+import { HttpClientModule }    from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 describe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule, FormsModule, HttpClientModule],
       declarations: [ SearchComponent ]
     })
     .compileComponents();
@@ -21,5 +23,14 @@ describe('SearchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display error message upon no input', () => {
+    fixture = TestBed.createComponent(SearchComponent);
+    component = fixture.componentInstance;
+    component.ngOnInit()
+    component.form.get('url').value = "";
+    component.getSites()
+    expect(component.output_text).toEqual("Please enter a valid url")
   });
 });
